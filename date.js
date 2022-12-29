4d9f2c96cbaad1fb7e46b95c5eefaab0471bd837
Suraj Charan <109950079+mylittleepiphanies100@users.noreply.github.com>
2022-12-29 15:26:18 +0530

    16

--- date.js
+++ date.js
@@ -3,7 +3,7 @@ let timeNow = new Date().getHours();
 let greeting =
   timeNow >= 5 && timeNow < 12
     ? "Good Morning"
-    : timeNow >= 12 && timeNow < 17
+    : timeNow >= 12 && timeNow < 16
     ? "Good Afternoon"
     : "Good evening";
 container.innerHTML = `<h4>${greeting}</h4>`;
